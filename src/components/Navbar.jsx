63b2e89a3486f9dd0ef0e8fd42f54e885d0a2589
daniatoudjibril <daniatoudjibril@gmail.com>
2022-11-01 16:48:45 +0100

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,42 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { UserContext } from '../context/userContext'
 import { Link, useNavigate } from 'react-router-dom'
 import { signOut } from 'firebase/auth'
 import { auth } from '../firebase-config'
 
 export default function Navbar() {
-    const {toggleModals} = useContext(UserContext)
+    const { toggleModals } = useContext(UserContext)
     const navigate = useNavigate();
 
     const logOut = async () => {
         try {
             await signOut(auth)
             navigate('/')
-        } catch{
+        } catch {
             alert('Nous rencontrons quelques problèmes. Réessayer')
         }
     }
 
 
     return (
-    <nav className='navbar navbar-light bg-light px-4'>
-        <Link to = "/" className='navbar-brand'>
-            Authentification
-        </Link>
+        <nav className='navbar   px-4'>
+            <Link to="/" className='navbar-brand text-light'>
+                Authentification
+            </Link>
 
-        <div>
-            <button onClick={() => toggleModals('signUp')} className="btn btn-primary">
-                Sign Up
-            </button>
-            <button onClick={() => toggleModals("signIn")} className="btn btn-primary ms-2">
-                Sign In
-            </button>
-            <button onClick={logOut}   className="btn btn-danger ms-2">
-               Log Out
-            </button>
-        </div>
-    </nav>
-   
- 
+            <div>
+                <button onClick={() => toggleModals('signUp')} className="btn btn-secondary">
+                    Sign Up
+                </button>
+                <button onClick={() => toggleModals("signIn")} className="btn btn-secondary ms-2">
+                    Sign In
+                </button>
+                <button onClick={logOut} className="btn btn-danger ms-2">
+                    Log Out
+                </button>
+            </div>
+        </nav>
+
+
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,36 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { UserContext } from '../context/userContext'
+import vector from './Vector.jpg'
 
 
 export default function Home() {
-  const {currentUser} = useContext(UserContext)
+  const { currentUser, toggleModals } = useContext(UserContext)
 
 
   return (
     <div className='container p-5'>
-        <h1 className='display-2 text-light' >
-          {currentUser? 'Welcome': '  Hello, Sign In or Sign Up' }
-        </h1>
+      <h1 className='display-2 text-light' >
+        {currentUser ? 'Welcome' :
+          <div className='container-fluid d-flex '>
+            <div className=" w-50 p-5">
+              <h1 className='display-5 fw-bold'>
+                WELCOME TO MY  REACT APP
+              </h1>
+              <p className='fs-6 text '>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, accusamus. Expedita eveniet rem, hic nam corrupti quaerat? Sit cupiditate iusto placeat veniam incidunt accusamus illo! Repellat facilis quisquam dolorum fuga.
+              </p>
+              <button onClick={() => toggleModals("signIn")} className="btn btn-secondary  ">
+                Sign In
+            </button>
+            </div>
+
+            <div className="logo-backg w-50 p-5">
+              <img className='w-100 rounded-3' src={vector} alt="Logo Background" />
+            </div>
+          </div>
+        }
+      </h1>
+
     </div>
   )
 }
